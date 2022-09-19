80da2f6efdcb33fdce90d3e2bd14fe3b51d3148f
septimis <connor.meads@protonmail.com>
2022-09-18 22:18:48 -0600

    added a pulsing arrow at the bottom of landing page.  Added the sign up page

--- dev/src/main/kotlin/com/budgetor/HomeController.kt
+++ dev/src/main/kotlin/com/budgetor/HomeController.kt
@@ -1,6 +1,7 @@
 package com.budgetor;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -13,20 +14,22 @@ public class HomeController {
 
     @GetMapping("/about")
     fun about() : String {
-        println("In about get mapping");
         return "landingFragments/about";
     }
 
     @GetMapping("/app")
     fun app() : String {
-        println("in app get mapping");
         return "landingFragments/app";
     }
 
     @GetMapping("/contact")
     fun contact() : String {
-        println("in contact get mapping");
         return "landingFragments/contact";
+    }
+
+    @GetMapping("/signUp")
+    fun signUp() : String {
+        return "landingFragments/signUp";
     }
 
     @GetMapping("/error")
---
+++ dev/src/main/resources/static/landing/js/signUp.js
@@ -0,0 +1,80 @@
+//pertinent DOM elements
+const firstNameInput = document.getElementById('firstNameInput');
+const lastNameInput = document.getElementById('lastNameInput');
+const phoneNumberInput = document.getElementById('phoneNumberInput');
+const emailInput = document.getElementById('emailInput');
+const passwordInput = document.getElementById('passwordInput');
+const passwordConfirmationInput = document.getElementById('passwordConfirmationInput');
+const inputElements = document.querySelectorAll('signUpFormInputs');
+const helpText = document.getElementById('helpText');
+const createAccountBtn = document.getElementById('createAccountBtn');
+
+let isValidInput = false;
+const phoneRegEx = /\(\d{3}\)\s\d{3}-\d{4}/;
+const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordRegEx =  /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/;
+
+//checks validity of input
+function validateInput() {
+    let errorMsg = '';
+    isValidInput = true;
+
+    //validate phone number
+    if(phoneNumberInput.value === '') phoneNumberInput.style.border = 'none';
+    else if(phoneRegEx.exec(phoneNumberInput.value)) {
+        phoneNumberInput.style.border = '1px solid green';
+    } else {
+        phoneNumberInput.style.border = '1px solid red';
+        isValidInput = false;
+        errorMsg = errorMsg + 'Phone number is invalid. Make sure it is of the form: (###) ###-####\n';
+    }
+
+    //email
+    if(emailInput.value === '') {
+        emailInput.style.border = 'none';
+        isValidInput = false;
+    } else if(emailRegEx.exec(emailInput.value)) {
+        emailInput.style.border = '1px solid green';
+    } else {
+        emailInput.style.border = '1px solid red';
+        errorMsg = errorMsg + 'Email Address is invalid.\n';
+        isValidInput = false;
+    }
+
+    //password
+    if(passwordInput.value === '') {
+        passwordInput.style.border = 'none';
+        isValidInput = false;
+    } else if(passwordRegEx.exec(passwordInput.value)) {
+        passwordInput.style.border = '1px solid green';
+    } else {
+        passwordInput.style.border = '1px solid red';
+        errorMsg = errorMsg + 'Password needs at least 1 lowercase letter, 1 uppercase letter, one digit, one special character and be at least 8 characters long\n';
+        isValidInput = false;
+    }
+
+    if(passwordConfirmationInput.value === '' && passwordInput.value === '') {
+        passwordConfirmationInput.style.border = 'none';
+    } else if(passwordConfirmationInput.value === passwordInput.value) {
+        passwordConfirmationInput.style.border = '1px solid green';
+    } else {
+        passwordConfirmationInput.style.border = '1px solid red';
+        errorMsg = errorMsg + 'Confirmation password must match password';
+        isValidInput = false;
+    }
+
+    helpText.innerText = errorMsg;
+    console.log(`isValidInput: ${isValidInput}`);
+    if(isValidInput) {
+        createAccountBtn.disabled = false;
+        createAccountBtn.classList.add('popAnimation');
+    } else {
+        createAccountBtn.disabled = true;
+        createAccountBtn.classList.remove('popAnimation');
+    }
+}
+
+function createAccount() {
+    if(!isValidInput) return;
+    alert('You created an account successfully!');
+}
