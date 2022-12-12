20df81f4bfefa2067cde02b0b6e143d3905c9fbd
septimis <connor.meads@live.com>
2022-12-12 14:29:02 -0700

    Features Added:   Finished the pull out menu.

--- dev/src/main/kotlin/com/budgetor/Controllers/HomeController.kt
+++ dev/src/main/kotlin/com/budgetor/Controllers/HomeController.kt
@@ -3,6 +3,7 @@ package com.budgetor.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 import org.springframework.security.access.prepost.PreAuthorize
 
 @Controller
@@ -11,7 +12,10 @@ public class HomeController {
 
     @PreAuthorize("hasRole('USER')")
     @GetMapping()
-    fun current() : String = "homeFragments/home.html";
+    fun current(homeModel : Model) : String {
+        homeModel.addAttribute("Name", "Paulina");
+        return "homeFragments/home.html";
+    }
 
     @PreAuthorize("hasRole('USER')")
     @GetMapping("/bigPicture")
